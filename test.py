3444c21199f56de4c5cbf18d3922f0fef8c77327
Clayton Kuchta <clayton.kuchta@gmail.com>
2015-11-01 21:52:31 +0000

    remove unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-from ZeroconfService import ZeroconfService
-import time
- 
-service = ZeroconfService(name="Joe's awesome FTP server",
-                          port=3000,  stype="_ftp._tcp")
-service.publish()
-time.sleep(10)
-service.unpublish()
-
